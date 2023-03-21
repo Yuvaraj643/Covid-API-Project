b1f2f29089ea8274418494aa2d7e8cbed322ee9e
Yuvaraj V <112630545+Yuvaraj643@users.noreply.github.com>
2023-03-21 06:49:59 +0530

    Add files via upload

--- src/Components/Covid_Data.js
+++ src/Components/Covid_Data.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { useState , useEffect } from 'react'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './Covid_Data.css'
 
 const Covid_Data = () => {
   const [data,setData]=useState([]);
+  const [search, setSearch] = useState("");
   const getCovidData = async () => {
     const res = await fetch('https://api.rootnet.in/covid19-in/stats/latest');
     const resultData = await res.json()
@@ -15,20 +18,37 @@ const Covid_Data = () => {
       getCovidData();
   }, []);
 
+  function handleChange(e) {
+    setSearch(e.target.value)
+  }
 
   return(
-    <div>
+    <>
       <div class="nav">Covid API Project</div>
+        <input
+          className="input"
+          type="text"
+          placeholder="     Search State "
+          value={search}
+          onChange={handleChange}
+/>
+      <div class='row row-cols-1 row-cols-md-4 g-4'>
       {
-        data.map((coin) =>{
+        data && data.filter(item => item.loc.includes(search)).map((coin) =>{
           return (
-            <div>
-              <h1>{coin.loc}</h1>
-            </div>
+              <div class="col">
+                <div class="card border-info mb-3 shadow p-3 mb-5">
+                  <span class="card-title">{coin.loc}</span>
+                  <p class="card-text"><span>Total Confirmed Cases </span>: {coin.confirmedCasesIndian}</p>
+                  <p class="card-text"><span>Total Deaths </span>: {coin.deaths}</p>
+                  <p class="card-text"><span>Discharged</span>: {coin.discharged}</p>
+                </div>
+              </div>
           )
         })
       }
     </div>
+    </>
 
   );
 }
